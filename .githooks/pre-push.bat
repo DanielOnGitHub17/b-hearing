8a495fd92ec37dbe1a9bcb8c484fabc0d5ab353d
Daniel Enesi <enesidaniel.120064@gmail.com>
2026-02-17 00:42:22 -0600

    Update checks to run Django Checks before push

--- .githooks/pre-push.bat
+++ .githooks/pre-push.bat
@@ -1,7 +1,12 @@
 @echo off
 echo Running checks before push...
 
+echo Checking migrations
 python manage.py makemigrations --check
+
+echo Running tests
+python manage.py test
+
 IF %ERRORLEVEL% NEQ 0 (
     echo Tests failed. Push aborted.
     exit /b 1
