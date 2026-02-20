b948c5fcbccb56c93a1624197924497996d875e9
Daniel Enesi <enesidaniel.120064@gmail.com>
2026-02-19 20:21:33 -0600

    Start VerseRange form creation, handling - for better input control

--- static/selections/form.js
+++ static/selections/form.js
@@ -1,5 +1,8 @@
 // alert("welcome");
-const addVerseRange = ("add-verse-range");
+const addVerseRange = get("add-verse-range");
+const verseRangeForms = get("verse-range-forms")
+const verseRanges = get("verse-ranges")
+
 // Use a field set
 // check that verse range doesn't start AND end from the same place
 // Maybe also indicate that a particular start or end has already been selected for this particular verse
@@ -7,3 +10,9 @@ const addVerseRange = ("add-verse-range");
 // For elsewhere: Selections should be editted the same way they are created.
 // The form will look the same.
 // Add validation for chapter based on Book and verse based on chapter. 
+onload = () => {
+    for (let x in ["start", "end"]) {
+        VerseRangeForm(x, verseRangeForms);
+    }
+}
+// build it twice for one and other
--- static/selections/selection.js
+++ static/selections/selection.js
@@ -1,6 +1,7 @@
 class Verse {
     constructor(book, chapter, verse, number, rangeType, parentElement) {
         for (let prop of args) this[prop] = args[prop];
+        // this
     }
 
     build() {
@@ -39,3 +40,44 @@ class VerseRange {
         reclass(add(this.verseRangeText = make(), this.parentElement), "verse-range-text");
     }
 }
+
+class VerseRangeForm {
+    constructor(name, parentElement) {
+        for (let prop of args) this[prop] = args[prop];
+        this.build();
+    }
+
+    build() {
+        // It's funny how the add(what, to) function looks like you're walking from inside to outside
+
+        // Build elements
+        add(this.container = make("fieldset"), to = this.parentElement).id = `${this.name}-verse-form`;
+
+        add(this.legend = make("legend"), to = this.container).legend = `Select ${this.name}ing verse`;
+
+        this.book = add(
+            make("select"),
+            to = add(make("label"), to = this.container)
+        );
+
+        ["chapter", "verse"].forEach(prop => {
+            let holder = make("label");
+            holder.textContent = `Select ${prop.toUpperCase()}`;
+            this[prop] = add(make("input"));
+        });
+
+        (this.chapter = add(
+            make("input"),
+            to = add(make("label"), to = this.container)
+        )).type = "number";
+
+        (this.verse = add(
+            make("input"),
+            to = add(make("label"), to = this.container)
+        )).type = "number";
+        // Add additional properties.
+    }
+    static forms = [];
+}
+
+
