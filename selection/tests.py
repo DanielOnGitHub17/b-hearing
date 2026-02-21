f6d24427345286040375b747e08aca8f30de04e8
Daniel Enesi <enesidaniel.120064@gmail.com>
2026-02-21 15:34:53 -0600

    Database testing

--- selection/tests.py
+++ selection/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .populate_verses import init_bible_db
+
+
+class TestInitDB(TestCase):
+    def test_init_db(self):
+        pass
+        # self.assertIsNone(
+        #     init_bible_db(
+        #         "kjv", r"C:\Users\enesi\Code\b-hearing\bible-data\kjv_curated.json"
+        #     )
+        # )
