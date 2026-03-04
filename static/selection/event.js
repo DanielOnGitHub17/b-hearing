7c33b3a24a12d7b57bd23507f817a3c918728757
Daniel Enesi <enesidaniel.120064@gmail.com>
2026-03-04 17:08:24 -0600

    Build Verses UI from backend loaded json

--- static/selection/event.js
+++ static/selection/event.js
@@ -1,1 +1,19 @@
+import { VerseRange } from "./VerseRange.js";
+// load voices,
+// build versesUI
 // Etc.
+
+
+function buildUI(e) {
+    // Add verse ranges, verses.
+    const verseRanges = get("verse-ranges");
+    const verseRangeObjs = JSON.parse(get("verse-ranges-obj").textContent);
+    for (const verseRangeObj of verseRangeObjs) {
+        const verseRange = new VerseRange(verseRangeObj, verseRanges);
+    }
+}
+
+configureEvents({
+    "load": [buildUI]
+})
+
