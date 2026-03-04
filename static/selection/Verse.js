0682062559bf62d3421f614971f778da84b13590
Daniel Enesi <enesidaniel.120064@gmail.com>
2026-03-04 17:08:43 -0600

    Format, persist verses

--- static/selection/Verse.js
+++ static/selection/Verse.js
@@ -1,18 +1,21 @@
 class Verse {
     constructor(verseObj, parentElement) {
-        for (const prop in verseObj) {
-            this[prop] = verseObj[prop];
-        }
+        for (const prop in verseObj) this[prop] = verseObj[prop];
         this.parentElement = parentElement;
         this.build();
+        Verse.verses.push(this);
     }
 
     build() {
-        this.container = add(make("p", { className: "verse-holder" }));
-        for (const prop of props) {
-            this[`${prop}Holder`] = make("span", { textContent: this[prop], className: prop });
+        this.container = add(make("p", { className: "verse-holder" }), this.parentElement);
+        for (const prop of Verse.props) {
+            this[`${prop}Holder`] =
+                add(make("span", { textContent: this[prop], className: prop }), this.container);
         }
     }
 
     static props = ["book", "chapter", "verse", "text"]
+    static verses = [];
 }
+
+export { Verse };
